26832afbe29a8bb2501ded3307a27902120bb204
Rajeshwar reddy <50393237+RajeshwarReddy48@users.noreply.github.com>
2022-07-27 19:35:01 +0530

    Delete Payment directory

--- Payment/src/main/java/com/casestudy/Payment/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.casestudy.Payment.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.casestudy.Payment.model.Payment;
-
-public interface PaymentRepository extends MongoRepository<Payment, String> {
-
-}
